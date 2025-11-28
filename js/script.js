56099c0889cc8152daddc71b08548a31b1e15f93
Livrikras <livrikras@icloud.com>
2025-11-28 13:35:15 +0100

    fået browseren til original størrelse

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("sec-1");
+  const prev = document.getElementById("sec1-prev");
+  const next = document.getElementById("sec1-next");
+
+  const scrollAmount = container.clientWidth * 0.8; // scroll 80% af synlig bredde
+
+  if (next)
+    next.addEventListener("click", () => {
+      container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+
+  if (prev)
+    prev.addEventListener("click", () => {
+      container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+});
