12f06a07777abbb2f30be5f911a6cbcecd500c99
Livrikras <livrikras@icloud.com>
2025-12-01 12:37:49 +0100

    valideret, js på sektion 1, scroll effekt i sektion 1 og 3

--- js/script.js
+++ js/script.js
@@ -1,19 +1,98 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("sec-1");
-  const prev = document.getElementById("sec1-prev");
-  const next = document.getElementById("sec1-next");
+// ----------------------hero---------------------
+const buyButton = document.getElementById("buy-btn");
 
-  const scrollAmount = container.clientWidth * 0.8; // scroll 80% af synlig bredde
+buyButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: document.getElementById("sec-1").offsetTop,
+    behavior: "smooth",
+  });
+});
 
-  if (next)
-    next.addEventListener("click", () => {
-      container.scrollBy({ left: scrollAmount, behavior: "smooth" });
-    });
+// -----------------------------------------------
+// ------------------sec1-------------------------
+const greenSpeaker = document.getElementById("g-speaker");
 
-  if (prev)
-    prev.addEventListener("click", () => {
-      container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
-    });
+greenSpeaker.addEventListener("mouseover", function() {
+  this.classList.add("scale");
+});
+
+greenSpeaker.addEventListener("mouseout", function () {
+  this.classList.remove("scale");
+});
+
+
+const purpleSpeaker = document.getElementById("p-speaker");
+
+purpleSpeaker.addEventListener("mouseover", function () {
+  this.classList.add("scale");
+});
+
+purpleSpeaker.addEventListener("mouseout", function () {
+  this.classList.remove("scale");
+});
+
+
+const whiteSpeaker = document.getElementById("w-speaker");
+
+whiteSpeaker.addEventListener("mouseover", function () {
+  this.classList.add("scale");
+});
+
+whiteSpeaker.addEventListener("mouseout", function () {
+  this.classList.remove("scale");
+});
+
+
+const rosaSpeaker = document.getElementById("r-speaker");
+
+rosaSpeaker.addEventListener("mouseover", function () {
+  this.classList.add("scale");
+});
+
+rosaSpeaker.addEventListener("mouseout", function () {
+  this.classList.remove("scale");
+});
+
+// -----------------------------------------------
+// ------------------sec-3------------------------
+
+// const section3 = document.getElementById("sec-3");
+
+// // Window tager fat om browservinduet og gør noget ved
+// window.onscroll = () => {
+//   section3.forEach((sec) => {
+//     // Får nuværende scrollposition
+//     let scrollDistance = window.scrollY;
+
+//     // Får sektionens position ift. toppen af dokumentet
+//     let secDistance = sec.offsetTop;
+
+//     // Hvis sektionen er kommet ind i synsfeltet, så tilføjes "show animate", eller fjernes den
+//     if (scrollDistance >= secDistance) {
+//       sec.classList.add("move");
+//     } else {
+//       sec.classList.remove("move");
+//     }
+//   });
+// };
+
+const container = document.querySelector("#sec-1 .scroll-sec1");
+
+container.querySelectorAll(".speaker").forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    // Gør item centralt i container scroll
+   
+  });
+});
+// -----------------------------------------------
+// ------------------Footter----------------------
+const upButton = document.getElementById("up-btn");
+
+upButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
